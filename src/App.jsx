263e690d8a2993e03b12d91306a44a32c644650f
Gethmi-Rathnayaka <gethmisrathnayaka@gmail.com>
2024-06-13 16:08:35 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import Header from './Components/Header.jsx'
+
 import Footer from './Components/Footer.jsx'
 import Sec1 from './Components/Sec1.jsx'
 import Sidebar from './Components/Sidebar.jsx'
+
 import './App.css'
 
 function App() {
